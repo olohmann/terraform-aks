57e93b5d990545f84af8cddda339e8e1cb8568c0
Oliver Lohmann <oliver@lohmann.io>
2019-03-01 21:08:52 +0100

    Role name casing details.

--- 01-aks/role_assignments.tf
+++ 01-aks/role_assignments.tf
@@ -8,6 +8,6 @@ resource "azurerm_role_assignment" "network_contributor" {
 resource "azurerm_role_assignment" "acr_pull" {
   count                = "${var.azure_container_registry_id != "" ? 1 : 0}"
   scope                = "${var.azure_container_registry_id}"
-  role_definition_name = "acrpull"
+  role_definition_name = "AcrPull"
   principal_id         = "${var.aks_cluster_sp_object_id}"
 }
