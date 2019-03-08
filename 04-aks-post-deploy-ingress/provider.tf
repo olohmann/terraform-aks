7ffb04518d0f0c650cb625548f2cb42ad2d7aee7
Oliver Lohmann <oliver@lohmann.io>
2019-03-08 22:14:04 +0100

    Remove tiller specific image.

--- 04-aks-post-deploy-ingress/provider.tf
+++ 04-aks-post-deploy-ingress/provider.tf
@@ -6,6 +6,5 @@ provider "helm" {
     version = ">=0.8.0"
     namespace = "kube-system"
     service_account = "tiller-sa"
-    tiller_image = "gcr.io/kubernetes-helm/tiller:v2.12.3"
     install_tiller = "true"
 }
