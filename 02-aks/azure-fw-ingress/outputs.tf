dc09c6f8876e80a75aba3ce0a8a9f973c65c4e90
Darius Tehrani <darius.tehrani@microsoft.com>
2019-04-22 01:11:58 +0200

    rt_id var

---
+++ 02-aks/azure-fw-ingress/outputs.tf
@@ -0,0 +1,3 @@
+output "aks-subnet-rt-id" {
+  value = "${azurerm_route_table.aks_subnet_rt.id}"
+}
--- 02-aks/network.tf
+++ 02-aks/network.tf
@@ -11,7 +11,7 @@ resource "azurerm_subnet" "aks_subnet" {
   resource_group_name  = "${azurerm_resource_group.rg.name}"
   address_prefix       = "${local.aks_subnet_cidr}"
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-  route_table_id       = "${azurerm_route_table.aks_subnet_rt.id}"
+  route_table_id       = "${module.azure-fw-ingress.aks-subnet-rt-id}"
 }
 
 resource "azurerm_subnet_route_table_association" "rt_association" {
