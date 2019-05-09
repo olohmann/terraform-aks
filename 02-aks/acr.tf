ae7b9901f43786b07c3a41bbe25bcd30ffd75ff4
Darius Tehrani <darius.tehrani@microsoft.com>
2019-05-09 12:51:13 +0200

    acr principal id

--- 02-aks/acr.tf
+++ 02-aks/acr.tf
@@ -11,5 +11,5 @@ resource "azurerm_role_assignment" "acr_dedicated_pull" {
   count                = "${var.create_azure_container_registry == "true" ? 1 : 0}"
   scope                = "${azurerm_container_registry.acr.id}"
   role_definition_name = "AcrPull"
-  principal_id         = "${var.aks_cluster_sp_object_id}"
+  principal_id         = "${azuread_service_principal.aks_app_sp.principal_id}"
 }
