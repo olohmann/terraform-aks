86992bdbdba5eeb1d4d65ad2b9a3fce37ee75ed2
Darius Tehrani <darius.tehrani@microsoft.com>
2019-05-09 09:23:08 +0000

    interim

--- create_aks_sp.sh
+++ create_aks_sp.sh
@@ -7,20 +7,20 @@ set -o pipefail
 
 read -p "Enter your AKS cluster name: " aksname
 
-# Create the Azure AD application
+echo "Creating the Azure AD application"
 serverApplicationId=$(az ad app create \
     --display-name "${aksname}Server" \
     --identifier-uris "https://${aksname}Server" \
     --query appId -o tsv)
 
-wait 10
+sleep 10s
 
-# Update the application group memebership claims
+echo "Updating the application group memebership claims"
 az ad app update --id $serverApplicationId --set groupMembershipClaims=All
 
-wait 5
+sleep 10s
 
-# Create a service principal for the Azure AD application
+echo "Create a service principal for the Azure AD application"
 az ad sp create --id $serverApplicationId
 
 # Get the service principal secret
@@ -29,6 +29,29 @@ serverApplicationSecret=$(az ad sp credential reset \
     --credential-description "AKSPassword" \
     --query password -o tsv)
 
+sleep 5s
 
+az ad app permission add \
+    --id $serverApplicationId \
+    --api 00000003-0000-0000-c000-000000000000 \
+    --api-permissions e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope 06da0dbc-49e2-44d2-8312-53f166ab848a=Scope 7ab1d382-f21e-4acd-a863-ba3e13f7da61=Role
 
+sleep 5s
 
+# Due to current AAD permission issue disabled
+# az ad app permission grant --id $serverApplicationId --api 00000003-0000-0000-c000-000000000000
+# az ad app permission admin-consent --id  $serverApplicationId
+
+echo "Creating Azure AD client component"
+clientApplicationId=$(az ad app create \
+    --display-name "${aksname}Client" \
+    --native-app \
+    --reply-urls "https://${aksname}Client" \
+    --query appId -o tsv)
+
+az ad sp create --id $clientApplicationId
+oAuthPermissionId=$(az ad app show --id $serverApplicationId --query "oauth2Permissions[0].id" -o tsv)
+
+az ad app permission add --id $clientApplicationId --api $serverApplicationId --api-permissions $oAuthPermissionId=Scope
+az ad app permission grant --id $clientApplicationId --api $serverApplicationId
+echo "Done."
