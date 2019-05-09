5ad558a81cd9fb465f0ed7f7ea15de7901fb59f5
Darius Tehrani <darius.tehrani@microsoft.com>
2019-05-09 12:54:36 +0200

    role assignment principal id

--- 02-aks/role_assignments.tf
+++ 02-aks/role_assignments.tf
@@ -2,12 +2,12 @@
 resource "azurerm_role_assignment" "network_contributor" {
   scope                = "${azurerm_subnet.aks_subnet.id}"
   role_definition_name = "Network Contributor"
-  principal_id         = "${var.aks_cluster_sp_object_id}"
+  principal_id         = "${azuread_service_principal.aks_app_sp.principal_id}"
 }
 
 resource "azurerm_role_assignment" "acr_pull" {
   count                = "${var.azure_container_registry_id != "" ? 1 : 0}"
   scope                = "${var.azure_container_registry_id}"
   role_definition_name = "AcrPull"
-  principal_id         = "${var.aks_cluster_sp_object_id}"
+  principal_id         = "${azuread_service_principal.aks_app_sp.principal_id}"
 }
