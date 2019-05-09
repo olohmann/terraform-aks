9c0c904bccf0d0f20938b3d9735596335504f989
Darius Tehrani <darius.tehrani@microsoft.com>
2019-05-09 19:45:43 +0200

    azure-fw-ingress, merged post-deploy into aks tf

--- 02-aks/azure-fw-ingress/provider.tf
+++ 02-aks/azure-fw-ingress/provider.tf
@@ -1,3 +1,19 @@
+provider "azurerm" {
+    version = "~>1.25.0"
+}
+
+provider "local" {
+    version = "~>1.1.0"
+}
+
+provider "random" {
+    version = "~>2.0.0"
+}
+
+provider "null" {
+    version = "~>2.1.0"
+}
+
 provider "kubernetes" {
     version = "~>1.5.1"
 }
@@ -6,6 +22,6 @@ provider "helm" {
     version = "~>0.8.0"
     namespace = "kube-system"
     service_account = "tiller-sa"
-    tiller_image = "gcr.io/kubernetes-helm/tiller:v2.13.1"
+    tiller_image = "gcr.io/kubernetes-helm/tiller:v2.12.3"
     install_tiller = "true"
 }
