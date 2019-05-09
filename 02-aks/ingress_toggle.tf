56c63de9994d9e19c73d1752c33c828055a7f9c4
Darius Tehrani <darius.tehrani@microsoft.com>
2019-05-09 16:10:36 +0200

    dependency issues

--- 02-aks/ingress_toggle.tf
+++ 02-aks/ingress_toggle.tf
@@ -8,7 +8,7 @@ module "azure-fw-ingress" {
   vnet_name = "${azurerm_virtual_network.vnet.name}"
   vnet_address_space = ["${azurerm_virtual_network.vnet.address_space.0}"]
   firewall_subnet_cidr = "${local.aks_subnet_cidr}"
-  la_monitor_containers_workspace_id = "${azurerm_log_analytics_workspace.la_monitor_containers.id}"
+  la_monitor_containers_workspace_id = "${azurerm_log_analytics_workspace.la_monitor_containers.workspace_id}"
   workspace_random_id = "${random_id.workspace.hex}"
 }
 
