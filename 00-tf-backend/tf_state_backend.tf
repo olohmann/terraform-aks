d469d23b4d66c1fc9e716ddb10eaa63cacd9e9f9
Darius Tehrani <darius.tehrani@microsoft.com>
2019-05-10 17:25:15 +0200

    backend did not use blob

--- 00-tf-backend/tf_state_backend.tf
+++ 00-tf-backend/tf_state_backend.tf
@@ -1,31 +1,13 @@
+locals {
+  prefix_snake = "${var.prefix}"
+}
 resource "local_file" "backend_config_aks" {
   content = <<EOF
-terraform {
-  backend "azurerm" {
-  resource_group_name  = "${var.resource_group_name}"
-  storage_account_name = "${var.storage_account_name}"
-  container_name       = "${var.storage_container_name}"
-  access_key           = "${var.storage_account_primary_access_key}"
-  key                  = "tfbackend.tfstate"
-  }
-}
-
-EOF
-
-  filename = "${path.module}/backend.tf"
-}
-
-resource "local_file" "backend_config_aks" {
-  content = <<EOF
-  terraform {
-  backend "azurerm" {
-  resource_group_name  = "${var.resource_group_name}"
-  storage_account_name = "${var.storage_account_name}"
-  container_name       = "${var.storage_container_name}"
-  access_key           = "${var.storage_account_primary_access_key}"
-  key                  = "aks.tfstate"
-  }
-}
+resource_group_name  = "${azurerm_resource_group.rg.name}"
+storage_account_name = "${azurerm_storage_account.sa.name}"
+container_name       = "${azurerm_storage_container.sc.name}"
+access_key           = "${azurerm_storage_account.sa.primary_access_key}"
+key                  = "aks.tfstate"
 EOF
 
   filename = "${path.module}/../01-aks/backend.tfvars"
@@ -33,33 +15,24 @@ EOF
 
 resource "local_file" "backend_config_aks_post_deploy" {
   content = <<EOF
-  terraform {
-  backend "azurerm" {
-  resource_group_name  = "${var.resource_group_name}"
-  storage_account_name = "${var.storage_account_name}"
-  container_name       = "${var.storage_container_name}"
-  access_key           = "${var.storage_account_primary_access_key}"
-  key                  = "aks_post_deploy.tfstate"
-  }
-}
-
+resource_group_name  = "${azurerm_resource_group.rg.name}"
+storage_account_name = "${azurerm_storage_account.sa.name}"
+container_name       = "${azurerm_storage_container.sc.name}"
+access_key           = "${azurerm_storage_account.sa.primary_access_key}"
+key                  = "aks_post_deploy.tfstate"
 EOF
 
-  filename = "${path.module}/../03-aks-post-deploy/backend.tf"
+  filename = "${path.module}/../03-aks-post-deploy/backend.tfvars"
 }
 
 resource "local_file" "backend_config_aks_post_deploy_ingress" {
   content = <<EOF
-  terraform {
-  backend "azurerm" {
-  resource_group_name  = "${var.resource_group_name}"
-  storage_account_name = "${var.storage_account_name}"
-  container_name       = "${var.storage_container_name}"
-  access_key           = "${var.storage_account_primary_access_key}"
-  key                  = "aks_post_deploy_ingress.tfstate"
-  }
-
+resource_group_name  = "${azurerm_resource_group.rg.name}"
+storage_account_name = "${azurerm_storage_account.sa.name}"
+container_name       = "${azurerm_storage_container.sc.name}"
+access_key           = "${azurerm_storage_account.sa.primary_access_key}"
+key                  = "aks_post_deploy_ingress.tfstate"
 EOF
 
-  filename = "${path.module}/../04-aks-post-deploy-ingress/backend.tf"
+  filename = "${path.module}/../04-aks-post-deploy-ingress/backend.tfvars"
 }
