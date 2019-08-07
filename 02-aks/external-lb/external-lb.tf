c4b77de8cb655187c93db72adaafa91c0fb593ea
Oliver Lohmann <oliver@lohmann.io>
2019-08-07 21:14:02 +0200

    Cosmetics.

--- 02-aks/external-lb/external-lb.tf
+++ 02-aks/external-lb/external-lb.tf
@@ -15,21 +15,6 @@ resource "azurerm_public_ip" "external_lb_pip" {
   domain_name_label   = "${var.prefix_snake}-${var.hash_suffix}"
 }
 
-// The actual ELB is not created here. We let AKS do it automatically in the MC_ RG.
-/*
-resource "azurerm_lb" "elb" {
-  count               = "${local.cnt_deploy}"
-  name                = "${var.prefix_snake}-elb"
-  resource_group_name = "${var.aks_node_resource_group_name}"
-  location            = "${var.resource_group_location}"
-
-  frontend_ip_configuration {
-    name                 = "configuration"
-    public_ip_address_id = "${var.external_pip_name == "" ? azurerm_public_ip.external_lb_pip.*.id[0]: local.external_pip_id}"
-  }
-}
-*/
-
 # Route Table: AKS Subnet -> Azure Firewall
 resource "azurerm_route_table" "aks_subnet_rt" {
   count               = "${local.cnt_deploy}"
--- 02-aks/role_assignments.tf
+++ 02-aks/role_assignments.tf
@@ -1,17 +1,17 @@
 /* ARM Role Assignments for AKS SP */
 resource "azurerm_role_assignment" "network_contributor" {
+  count                = "${var.assign_roles == "true" ? 1 : 0}"
   scope                = "${azurerm_subnet.aks_subnet.id}"
   role_definition_name = "Network Contributor"
   principal_id         = "${var.aks_cluster_sp_object_id}"
-  count                = "${var.assign_roles == "true" ? 1 : 0}"
 }
 
 /* Required to support an external ELB with a static IP as an alternative to the Azure Firewall */
 resource "azurerm_role_assignment" "network_contributor_resource_group" {
+  count                = "${var.assign_roles == "true" ? (var.deploy_azure_firewall == "true" ? 0 : 1) : 0}"
   scope                = "${azurerm_resource_group.rg.id}"
   role_definition_name = "Network Contributor"
   principal_id         = "${var.aks_cluster_sp_object_id}"
-  count                = "${var.assign_roles == "true" ? (var.deploy_azure_firewall == "true" ? 0 : 1) : 0}"
 }
 
 /* Data Role Assignment for AKS SP */
