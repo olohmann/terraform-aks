2a0ba0b27959e219b9ccf4cad85e9fa5360ea85c
Oliver Lohmann <oliver@lohmann.io>
2019-08-09 20:01:34 +0200

    Remove local helm client init. helm init is now executed by the terraform provider.

--- 04-aks-post-deploy-ingress/helm-init.sh
+++ 04-aks-post-deploy-ingress/helm-init.sh
@@ -10,9 +10,5 @@ STDIN=$(cat -)
 # Env Name is passed as a json string
 ENV_NAME=$(echo ${STDIN} | jq -c -r '.env' | tr -d '\n')
 
-# Create a local Helm Config Dir and init the client 
-export HELM_HOME="${DIR}/.helm_${ENV_NAME}/"
-helm init --client-only > /dev/null 2>&1
-
-# Return the path to the local HELM_HOME
+# Return the calculated path to the local HELM_HOME
 echo "{\"helm_home\": \"${HELM_HOME}\"}"
