88b3f9751e17860b8592740a6f3e6d64264d3367
Oliver Lohmann <oliver@lohmann.io>
2020-06-17 20:25:18 +0200

    Pull secret.

--- 01-aks/acr_pull_secret.tf
+++ 01-aks/acr_pull_secret.tf
@@ -1,4 +1,5 @@
 resource "kubernetes_secret" "acr_pull_secret_external_cr" {
+  // Only deploy the Pull Secret for the EXTERNAL ACR, if pull secret is enabled.
   count = var.use_external_azure_container_registry ? (var.deploy_container_registry_secret ? 1 : 0) : 0
 
   metadata {
@@ -21,7 +22,8 @@ resource "kubernetes_secret" "acr_pull_secret_external_cr" {
 }
 
 resource "kubernetes_secret" "acr_pull_secret" {
-  count = var.deploy_container_registry_secret ? 1 : 0
+  // Only deploy the Pull Secret for the INTERNAL ACR, if pull secret is enabled.
+  count = var.deploy_azure_container_registry ? (var.deploy_container_registry_secret ? 1 : 0) : 0
 
   metadata {
     name = "acr-pull-secret"
